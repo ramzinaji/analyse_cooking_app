142c181f811c589f25826080bfb11740d575ef94
nawfaladil <144803416+nawfaladil@users.noreply.github.com>
2024-12-05 16:40:47 +0100

    Update test_seasonality_checker.py

--- etude_app_cuisine/tests/test_seasonality_checker.py
+++ etude_app_cuisine/tests/test_seasonality_checker.py
@@ -24,23 +24,13 @@ def checker(dico_all_month_ingredient):
 # --- Tests pour la méthode is_seasonal ---
 def test_is_seasonal_good_choice(checker):
     """Test pour vérifier le meilleur mois d'un ingrédient."""
-    result = checker.is_seasonal('carrot', 4)
-    assert result == "Good choice, the best season to cook carrot is April."
-
-def test_is_seasonal_wrong_month(checker):
-    """Test pour vérifier si l'ingrédient n'est pas dans la bonne saison."""
-    result = checker.is_seasonal('carrot', 1)
-    assert result == "You should not cook carrot in January."
+    result = checker.is_seasonal('carrot')
+    assert result == "You should  cook carrot in April."
 
 def test_is_seasonal_not_in_database(checker):
     """Test pour vérifier le comportement avec un ingrédient absent."""
-    result = checker.is_seasonal('mango', 4)
+    result = checker.is_seasonal('mango')
     assert result == "Your ingredient 'mango' is not in our database."
-
-def test_is_seasonal_invalid_month(checker):
-    """Test pour capturer une exception lors d'une mauvaise entrée de mois."""
-    result = checker.is_seasonal('carrot', 13)
-    assert "An error occurred" in result
 
 # --- Tests pour la méthode ingredient_std ---
 def test_ingredient_std_normalized_values(checker):
