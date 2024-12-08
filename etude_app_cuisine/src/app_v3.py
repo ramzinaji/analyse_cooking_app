bb32f9cb8a87dd4abebe84db138de8052871fd49
nawfaladil <144803416+nawfaladil@users.noreply.github.com>
2024-12-08 19:36:37 +0100

    Update app_v3.py

--- etude_app_cuisine/src/app_v3.py
+++ etude_app_cuisine/src/app_v3.py
@@ -31,6 +31,11 @@ st.set_page_config(
     layout="wide"
 )
 
+st.page_link("app_v3.py", label="Home", icon="🏠")
+st.page_link("pages/page1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page2.py", label="Page 2", icon="2️⃣")
+st.page_link("http://www.google.com", label="Google", icon="🌎")
+
 # Log page load
 logging.info("Main page loaded.")
 
